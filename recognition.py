c559cd4fc4c306706e71d2dfe1beeb3d34528ed5
BanulaMahen <banulakumarage@gmail.com>
2023-04-23 02:29:20 +0530

    Formatting code

--- recognition.py
+++ recognition.py
@@ -16,8 +16,8 @@ resnet = InceptionResnetV1(pretrained='vggface2').eval()
 # Using webcam recognize face
 
 # loading data.pt file
-load_data = torch.load('data.pt') 
-embedding_list = load_data[0] 
+load_data = torch.load('data.pt')
+embedding_list = load_data[0]
 name_list = load_data[1] 
 
 cam = cv2.VideoCapture(1) 
@@ -27,45 +27,54 @@ while True:
     if not ret:
         print("fail to grab frame, try again")
         break
-        
+
     img = Image.fromarray(frame)
     img_cropped_list, prob_list = mtcnn(img, return_prob=True) 
-    
+
     if img_cropped_list is not None:
         boxes, _ = mtcnn.detect(img)
-                
+
         for i, prob in enumerate(prob_list):
             if prob>0.90:
                 emb = resnet(img_cropped_list[i].unsqueeze(0)).detach() 
-                
+
                 dist_list = [] # list of matched distances, minimum distance is used to identify the person
-                
-                for idx, emb_db in enumerate(embedding_list):
+
+                for emb_db in embedding_list:
                     dist = torch.dist(emb, emb_db).item()
                     dist_list.append(dist)
 
                 min_dist = min(dist_list) # get minumum dist value
                 min_dist_idx = dist_list.index(min_dist) # get minumum dist index
                 name = name_list[min_dist_idx] # get name corrosponding to minimum dist
-                
+
                 box = boxes[i] 
-                
+
                 original_frame = frame.copy() # storing copy of frame before drawing on it
-                
+
                 if min_dist<0.90:
                     box = box.astype(int)
-                    frame = cv2.putText(frame, name+' '+str(min_dist), (box[0],box[1]), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0),1, cv2.LINE_AA)
-                
+                    frame = cv2.putText(
+                        frame,
+                        f'{name} {str(min_dist)}',
+                        (box[0], box[1]),
+                        cv2.FONT_HERSHEY_SIMPLEX,
+                        1,
+                        (0, 255, 0),
+                        1,
+                        cv2.LINE_AA,
+                    )
+
                 box = box.astype(int)
                 frame = cv2.rectangle(frame, (box[0],box[1]) , (box[2],box[3]), (255,0,0), 2)
 
     cv2.imshow("IMG", frame)
-        
-    
+
+
     k = cv2.waitKey(1)
     if k%256==27: # ESC
         print('Esc pressed, closing...')
         break        
-        
+
 cam.release()
 cv2.destroyAllWindows()
