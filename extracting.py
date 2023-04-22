ffdc94cdd0c8028271841b4df99d09f446646b33
BanulaMahen <banulakumarage@gmail.com>
2023-04-23 02:19:41 +0530

    Using facenet_pytorch

--- extracting.py
+++ extracting.py
@@ -1,33 +1,38 @@
-from imutils import paths
-import face_recognition
-import pickle
+from facenet_pytorch import MTCNN, InceptionResnetV1
+import torch
+from torchvision import datasets
+from torch.utils.data import DataLoader
+from PIL import Image
 import cv2
+import time
 import os
- 
-#get paths of each file in folder named Images
-#Images here contains my data(folders of various persons)
-imagePaths = list(paths.list_images('Images'))
-knownEncodings = []
-knownNames = []
-# loop over the image paths
-for (i, imagePath) in enumerate(imagePaths):
-    # extract the person name from the image path
-    name = imagePath.split(os.path.sep)[-2]
-    # load the input image and convert it from BGR (OpenCV ordering)
-    # to dlib ordering (RGB)
-    image = cv2.imread(imagePath)
-    rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    #Use Face_recognition to locate faces
-    boxes = face_recognition.face_locations(rgb,model='hog')
-    # compute the facial embedding for the face
-    encodings = face_recognition.face_encodings(rgb, boxes)
-    # loop over the encodings
-    for encoding in encodings:
-        knownEncodings.append(encoding)
-        knownNames.append(name)
-#save emcodings along with their names in dictionary data
-data = {"encodings": knownEncodings, "names": knownNames}
-#use pickle to save data into a file for later use
-f = open("face_enc", "wb")
-f.write(pickle.dumps(data))
-f.close()
+
+# initializing MTCNN and InceptionResnetV1 
+
+mtcnn0 = MTCNN(image_size=240, margin=0, keep_all=False, min_face_size=40) # keep_all=False
+mtcnn = MTCNN(image_size=240, margin=0, keep_all=True, min_face_size=40) # keep_all=True
+resnet = InceptionResnetV1(pretrained='vggface2').eval() 
+
+# Read data from folder
+
+dataset = datasets.ImageFolder('Images') # Images folder path 
+idx_to_class = {i:c for c,i in dataset.class_to_idx.items()} # accessing names of peoples from folder names
+
+def collate_fn(x):
+    return x[0]
+
+loader = DataLoader(dataset, collate_fn=collate_fn)
+
+name_list = [] # list of names corrospoing to cropped Images
+embedding_list = [] # list of embeding matrix after conversion from cropped faces to embedding matrix using resnet
+
+for img, idx in loader:
+    face, prob = mtcnn0(img, return_prob=True) 
+    if face is not None and prob>0.92:
+        emb = resnet(face.unsqueeze(0)) 
+        embedding_list.append(emb.detach()) 
+        name_list.append(idx_to_class[idx])        
+
+# save data
+data = [embedding_list, name_list] 
+torch.save(data, 'data.pt') # saving data.pt file
--- recognition.py
+++ recognition.py
@@ -1,83 +1,71 @@
-import face_recognition
-import imutils
-import pickle
-import time
+from facenet_pytorch import MTCNN, InceptionResnetV1
+import torch
+from torchvision import datasets
+from torch.utils.data import DataLoader
+from PIL import Image
 import cv2
+import time
 import os
 
-total_comparisons = 0
-total_matches = 0
- 
-#find path of xml file containing haarcascade file 
-cascPathface = os.path.dirname(
- cv2.__file__) + "/data/haarcascade_frontalface_alt2.xml"
-# load the harcaascade in the cascade classifier
-faceCascade = cv2.CascadeClassifier(cascPathface)
-# load the known faces and embeddings saved in last file
-data = pickle.loads(open('face_enc', "rb").read())
- 
-print("Streaming started")
-video_capture = cv2.VideoCapture(1)
-# loop over frames from the video file stream
+# initializing MTCNN and InceptionResnetV1 
+
+mtcnn0 = MTCNN(image_size=240, margin=0, keep_all=False, min_face_size=40) # keep_all=False
+mtcnn = MTCNN(image_size=240, margin=0, keep_all=True, min_face_size=40) # keep_all=True
+resnet = InceptionResnetV1(pretrained='vggface2').eval() 
+
+# Using webcam recognize face
+
+# loading data.pt file
+load_data = torch.load('data.pt') 
+embedding_list = load_data[0] 
+name_list = load_data[1] 
+
+cam = cv2.VideoCapture(1) 
+
 while True:
-    # grab the frame from the threaded video stream
-    ret, frame = video_capture.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = faceCascade.detectMultiScale(gray,
-                                         scaleFactor=1.1,
-                                         minNeighbors=5,
-                                         minSize=(60, 60),
-                                         flags=cv2.CASCADE_SCALE_IMAGE)
- 
-    # convert the input frame from BGR to RGB 
-    rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    # the facial embeddings for face in input
-    encodings = face_recognition.face_encodings(rgb)
-    names = []
-    # loop over the facial embeddings incase
-    # we have multiple embeddings for multiple fcaes
-    for encoding in encodings:
-       #Compare encodings with encodings in data["encodings"]
-       #Matches contain array with boolean values and True for the embeddings it matches closely
-       #and False for rest
-        matches = face_recognition.compare_faces(data["encodings"],
-         encoding)
-        #set name =inknown if no encoding matches
-        name = "Unknown"
-        # check to see if we have found a match
-        if True in matches:
-            #Find positions at which we get True and store them
-            matchedIdxs = [i for (i, b) in enumerate(matches) if b]
-            counts = {}
-            # loop over the matched indexes and maintain a count for
-            # each recognized face face
-            for i in matchedIdxs:
-                #Check the names at respective indexes we stored in matchedIdxs
-                name = data["names"][i]
-                #increase count for the name we got
-                counts[name] = counts.get(name, 0) + 1
-            #set name which has highest count
-            name = max(counts, key=counts.get)
-
-            total_matches += 1
-        total_comparisons += len(matches)  # increment the total number of comparisons made
- 
- 
-        # update the list of names
-        names.append(name)
-        # loop over the recognized faces
-        for ((x, y, w, h), name) in zip(faces, names):
-            # rescale the face coordinates
-            # draw the predicted face name on the image
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            cv2.putText(frame, name, (x, y), cv2.FONT_HERSHEY_SIMPLEX,
-             0.75, (0, 255, 0), 2)
-    cv2.imshow("Frame", frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    ret, frame = cam.read()
+    if not ret:
+        print("fail to grab frame, try again")
         break
-video_capture.release()
-cv2.destroyAllWindows()
+        
+    img = Image.fromarray(frame)
+    img_cropped_list, prob_list = mtcnn(img, return_prob=True) 
+    
+    if img_cropped_list is not None:
+        boxes, _ = mtcnn.detect(img)
+                
+        for i, prob in enumerate(prob_list):
+            if prob>0.90:
+                emb = resnet(img_cropped_list[i].unsqueeze(0)).detach() 
+                
+                dist_list = [] # list of matched distances, minimum distance is used to identify the person
+                
+                for idx, emb_db in enumerate(embedding_list):
+                    dist = torch.dist(emb, emb_db).item()
+                    dist_list.append(dist)
 
-if total_comparisons > 0:
-    accuracy = total_matches / total_comparisons
-    print (f"{total_matches}/{total_comparisons}")
+                min_dist = min(dist_list) # get minumum dist value
+                min_dist_idx = dist_list.index(min_dist) # get minumum dist index
+                name = name_list[min_dist_idx] # get name corrosponding to minimum dist
+                
+                box = boxes[i] 
+                
+                original_frame = frame.copy() # storing copy of frame before drawing on it
+                
+                if min_dist<0.90:
+                    box = box.astype(int)
+                    frame = cv2.putText(frame, name+' '+str(min_dist), (box[0],box[1]), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0),1, cv2.LINE_AA)
+                
+                box = box.astype(int)
+                frame = cv2.rectangle(frame, (box[0],box[1]) , (box[2],box[3]), (255,0,0), 2)
+
+    cv2.imshow("IMG", frame)
+        
+    
+    k = cv2.waitKey(1)
+    if k%256==27: # ESC
+        print('Esc pressed, closing...')
+        break        
+        
+cam.release()
+cv2.destroyAllWindows()
